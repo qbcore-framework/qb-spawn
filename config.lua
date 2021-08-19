edeb7f63a5455950999f9fa5a1b411def489318f
vanillaa <80897991+vanillla1@users.noreply.github.com>
2021-08-19 12:04:47 +0300

    update vector4

--- config.lua
+++ config.lua
@@ -2,45 +2,25 @@ QB = {}
 
 QB.Spawns = {
     ["legion"] = {
-        coords = {
-            x = 195.17, 
-            y = -933.77, 
-            z = 29.7, 
-            h = 144.5 
-        },
+        vector4(195.17, -933.77, 29.7, 144.5),
         location = "legion",
         label = "Legion Square",
     },
 
     ["policedp"] = {
-        coords = {
-            x = 428.23, 
-            y = -984.28, 
-            z = 29.76, 
-            h = 3.5 
-        },
+        vector4(428.23, -984.28, 29.76, 3.5),
         location = "policedp",
         label = "Police Department",
     },
 
     ["paleto"] = {
-        coords = {
-            x = 80.35, 
-            y = 6424.12, 
-            z = 31.67, 
-            h = 45.5 
-        },
+        vector4(80.35, 6424.12, 31.67, 45.5),
         location = "paleto",
         label = "Paleto Bay",
     },
 
     ["motel"] = {
-        coords = {
-            x = 327.56, 
-            y = -205.08, 
-            z = 53.08, 
-            h = 163.5 
-        }, 
+        vector4(327.56, -205.08, 53.08, 163.5),
         location = "motel",
         label = "Motels",
     },
