98da146741c81080d32552fb29f763023311c9f9
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-10-24 11:21:15 -0600

    chore(oxmysql): Replace fetch

--- server.lua
+++ server.lua
@@ -2,7 +2,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 QBCore.Functions.CreateCallback('qb-spawn:server:getOwnedHouses', function(source, cb, cid)
     if cid ~= nil then
-        local houses = exports.oxmysql:fetchSync('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
+        local houses = exports.oxmysql:executeSync('SELECT * FROM player_houses WHERE citizenid = ?', {cid})
         if houses[1] ~= nil then
             cb(houses)
         else
