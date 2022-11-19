2a5af55469b5b7c3b4130f349334d37c9ed0ab06
Charlie <dev@charlie.fyi>
2022-11-19 12:18:49 +0000

    Bump version / Tidy

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,26 +2,27 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-Spawn'
-version '1.0.0'
+version '1.2.0'
 
 shared_scripts {
-	'config.lua',
-	'@qb-apartments/config.lua'
+    'config.lua',
+    '@qb-apartments/config.lua'
 }
 
 client_script 'client.lua'
+
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server.lua'
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
 }
 
 ui_page 'html/index.html'
 
 files {
-	'html/index.html',
-	'html/style.css',
-	'html/vue.js',
-	'html/reset.css'
+    'html/index.html',
+    'html/style.css',
+    'html/vue.js',
+    'html/reset.css'
 }
 
 lua54 'yes'
